68715e800da4293b02d3cdd1a16985f8a7a8414e
Rohit Rathod <rohitrathod7032@gmail.com>
2022-07-03 14:11:30 +0530

    size of data types in java

---
+++ _general/SizeOfdatatype.java
@@ -0,0 +1,16 @@
+package _general;
+
+import java.sql.SQLOutput;
+
+public class SizeOfdatatype {
+    public static void main(String[] args){
+        System.out.println(Integer.SIZE/8);
+        System.out.println(Character.SIZE/8);
+        System.out.println(Float.SIZE/8);
+        System.out.println(Long.SIZE/8);
+        System.out.println(Double.SIZE/8);
+        System.out.println(Short.SIZE/8);
+        System.out.println(Byte.SIZE/8);
+
+    }
+}
