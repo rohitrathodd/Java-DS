e78fee9d35d9b9fc500b0b5a7be2515627425ffa
Rohit Rathod <rohitrathod7032@gmail.com>
2022-07-19 14:37:15 +0530

    adding shape class and its inheritance

---
+++ _general/Rectangle.java
@@ -0,0 +1,22 @@
+package _general;
+
+public class Rectangle extends Shape{
+
+    private int length;
+    private int width;
+
+    public Rectangle(){
+        this(0,0,0,0)
+    }
+
+    public Rectangle(int length,int width){
+        this(0,0,length,width);
+
+    }
+
+    public Rectangle(int x, int y, int length,int width){
+        super(x,y);
+        this.length=length;
+        this.width=width;
+    }
+}
---
+++ _general/Shape.java
@@ -0,0 +1,15 @@
+package _general;
+
+public class Shape {
+    private int x;
+    private int y;
+
+    public Shape(){
+        this(0,0);
+    }
+    public Shape(int x , int y) {
+        this.x = x;
+        this.y=y;
+    }
+
+}
